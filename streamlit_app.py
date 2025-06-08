c970b0e2952fdebf396d3f9a2023181344719c42
Too Yun Jie <46297525+WERTQY@users.noreply.github.com>
2025-06-08 18:41:38 +0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -68,7 +68,7 @@ class FeaturePipeline:
 # -------------------- Load model & coords --------------------
 @st.cache(allow_output_mutation=True)
 def load_model_and_pipeline():
-    model = load("model.joblib")
+    model = load("final_lightgbm_model.joblib")
     try:
         station_coords = load("station_coords.joblib")
     except Exception:
